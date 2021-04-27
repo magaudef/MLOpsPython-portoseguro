ad4a11e070b5387a976a4ffd4706cd4723ede81e
magaudef <magaudef@microsoft.com>
2021-04-27 15:32:02 +0000

    create new custom container

--- portoseguro/training/test_train.py
+++ portoseguro/training/test_train.py
@@ -1,4 +1,4 @@
-#import lightgbm
+import lightgbm
 import numpy as np
 import pandas as pd
 from portoseguro.training.train import split_data
@@ -13,19 +13,15 @@ def test_split_data():
         'col1': [1, 2, 3, 4, 5],
         'col2': [2, 1, 1, 2, 1]
         }
-
     data_df = pd.DataFrame(data=test_data)
     data = split_data(data_df)
-
     # verify that columns were removed correctly
     assert "target" not in data[0].data.columns
     assert "id" not in data[0].data.columns
     assert "col1" in data[0].data.columns
-
     # verify that data was split as desired
     assert data[0].data.shape == (4, 2)
     assert data[1].data.shape == (1, 2)
-
     # the valid_data set's raw data is used for metric calculation, so
     # free_raw_data should be False
     assert not data[1].free_raw_data
@@ -33,15 +29,12 @@ def test_split_data():
 
 def test_train_model():
     data = __get_test_datasets()
-
     params = {
         "learning_rate": 0.05,
         "metric": "auc",
         "min_data": 1
     }
-
     model = train_model(data, params)
-
     # verify that parameters are passed in to the model correctly
     for param_name in params.keys():
         assert param_name in model.params
@@ -50,15 +43,11 @@ def test_train_model():
 
 def test_get_model_metrics():
     class MockModel:
-
         @staticmethod
         def predict(data):
             return np.array([0, 0])
-
     data = __get_test_datasets()
-
     metrics = get_model_metrics(MockModel(), data)
-
     # verify that metrics is a dictionary containing the auc value.
     assert "auc" in metrics
     auc = metrics["auc"]
@@ -71,8 +60,7 @@ def __get_test_datasets():
     y_train = np.array([1, 1, 0, 1, 0, 1])
     X_test = np.array([7, 8]).reshape(-1, 1)
     y_test = np.array([0, 1])
-
-    #train_data = lightgbm.Dataset(X_train, y_train)
-    #valid_data = lightgbm.Dataset(X_test, y_test)
+    train_data = lightgbm.Dataset(X_train, y_train)
+    valid_data = lightgbm.Dataset(X_test, y_test)
     data = (train_data, valid_data)
     return data
